3e5034f4099400ac2a97bc36f9bf7a28e3291bf0
Zach Liibbe <zliibbe@gmail.com>
2022-04-26 11:33:26 -0600

    Add test file

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -7,6 +7,7 @@ const UrlContainer = props => {
     return (
       <div  key={url.id} className="url">
         <h3>{url.title}</h3>
+        <p className={`${url.id}`}>Id: {`${url.id}`}</p>
         
         <a href={url.short_url} target="blank">{url.short_url}</a>
         <p>{url.long_url}</p>
